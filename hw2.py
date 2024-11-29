13cfe788037d8b18eb0e001bdf866780d16ff859
StellarSynapse <skorubskijdmitro@gmail.com>
2024-11-29 20:53:29 +0200

    The second homework

---
+++ hw2.py
@@ -0,0 +1,25 @@
+#Task 1
+name = "Dmytro"
+
+day = "29/11/2024"
+
+print ("Good day, {}! {} is a perfect day to learn some python.".format(name, day))
+
+#Task 2
+
+firstName = "Dmytro"
+lastName = " Skorubskyi"
+
+print(firstName+lastName)
+
+#Task 3
+a = 3
+b = 4
+
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a**b)
+print(a%b)
+print(a//b)
